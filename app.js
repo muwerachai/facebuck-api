84064e80155a62b5f2966e1094decd75dc26b4a3
werachai <werachai@MacBook-Air-khxng-werachai.local>
2022-09-12 22:34:44 +0700

    add signup

--- app.js
+++ app.js
@@ -3,13 +3,23 @@ const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
 
+const authRouter = require('./routes/authRoute');
+const notFoundMiddleware = require('./middlewares/notFound');
+const errorMiddleware = require('./middlewares/error');
+
+// const {sequelize } = require('./models');
+// sequelize.sync({force: true});
+
 const app = express();
 app.use(cors());
-if (process.env.NODE_ENV === 'DEVELOPMENT') {
+
+if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'));
 }
 app.use(express.json());
 app.use(express.urlencoded({ extended: false}));
+
+app.use('/auth',authRouter);
 
 app.use(notFoundMiddleware);
 app.use(errorMiddleware);
