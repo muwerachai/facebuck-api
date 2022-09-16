bd54f4689db2fe9381d8fad7032863d6b1754256
werachai <werachai@MacBook-Air-khxng-werachai.local>
2022-09-16 13:13:16 +0700

    add profile upload

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,8 @@
+const cloudinary = require('cloudinary').v2;
+const fs = require('fs');
+const { User } = require('../models')
 const FriendService = require("../services/friendService");
+
 
 exports.getMe = async (req,res) => {
    const user = JSON.parse(JSON.stringify(req.user)); 
@@ -6,3 +10,23 @@ exports.getMe = async (req,res) => {
    user.friends = friends;
     res.json({ user});
 };
+
+exports.updateProfile = async (req,res,next) => {
+    try{
+        console.log(req.file);
+        cloudinary.uploader.upload(req.file.path, async (error, result) => {
+            if(error) {
+            return next(error);
+            }
+               await User.update(
+            { profilePic: result.secure_url }, 
+            { where: {id: req.user.id }}
+        );
+        fs.unlinkSync(req.file.path);
+        res.json({ profilePic: result.secure_url });
+        });
+     
+    }catch(err){
+        next(err);
+    }
+ };
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const userController = require('../controllers/userController');
+const upload = require('../middlewares/upload');
 
 const router = express.Router();
 
 router.get('/me' , userController.getMe);
+router.patch('/' , upload.single('profilePic'), userController.updateProfile);
 
 
 module.exports = router;
